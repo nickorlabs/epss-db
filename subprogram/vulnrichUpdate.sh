cc81ad2daad6bf82531ad5788da147d93ffc1d72
Your Name <you@example.com>
2025-05-01 18:06:22 +0000

    Fix floating point comparison in vulnrichUpdate.sh and update test-scripts.sh path detection

--- subprogram/vulnrichUpdate.sh
+++ subprogram/vulnrichUpdate.sh
@@ -21,7 +21,7 @@ ssvcTech=`jq -r '.containers.adp[]?.metrics[]?.other? | select(.type=="ssvc") | 
 
 # KEV
 kevDate=$(jq -r '.containers.adp[]?.metrics[]?.other? | select(.type == "kev") | .content?.dateAdded' "$infile")
-if [ -z $kevDate ] ; then
+if [ -z "$kevDate" ] ; then
   kevDate="1900-01-01"
 fi
 
@@ -29,7 +29,8 @@ kevRef=`jq -r '.containers.adp[]?.metrics[]?.other? | select(.type=="kev") | .co
 
 # adp.cvssV31
 adpV31score=`jq -r '.containers.adp[]?.metrics[]? | select(.cvssV3_1) | .cvssV3_1.baseScore' $infile`
-if [ -z $adpV31score ] ; then
+# Fix: quote the variable and use pattern matching instead of -z test for numbers
+if [ "$adpV31score" = "" ] || [ "$adpV31score" = "null" ] ; then
   adpV31score="0"
 fi
 
@@ -38,7 +39,8 @@ adpV31vector=`jq -r '.containers.adp[]?.metrics[]? | select(.cvssV3_1) | .cvssV3
 
 # cna.cvssV31
 cnaV31score=`jq -r '.containers.cna.metrics[]? | select(.cvssV3_1) | .cvssV3_1.baseScore' $infile`
-if [ -z $cnaV31score ] ; then
+# Fix: quote the variable and use pattern matching instead of -z test for numbers
+if [ "$cnaV31score" = "" ] || [ "$cnaV31score" = "null" ] ; then
   cnaV31score="0"
 fi
 cnaV31severity=`jq -r '.containers.cna.metrics[]? | select(.cvssV3_1) | .cvssV3_1.baseSeverity' $infile`
@@ -46,7 +48,8 @@ cnaV31vector=`jq -r '.containers.cna.metrics[]? | select(.cvssV3_1) | .cvssV3_1.
 
 # cna.cvssV40
 cnaV40score=`jq -r '.containers.cna.metrics[]? | select(.cvssV4_0) | .cvssV4_0.baseScore' $infile`
-if [ -z $cnaV40score ] ; then
+# Fix: quote the variable and use pattern matching instead of -z test for numbers
+if [ "$cnaV40score" = "" ] || [ "$cnaV40score" = "null" ] ; then
   cnaV40score="0"
 fi
 
@@ -55,4 +58,3 @@ cnaV40vector=`jq -r '.containers.cna.metrics[]? | select(.cvssV4_0) | .cvssV4_0.
 
 # output
 echo "\"$cveid\",\"$cweid\",\"$ssvcExpl\",\"$ssvcAuto\",\"$ssvcTech\",\"$kevDate\",\"$kevRef\",\"$adpV31score\",\"$adpV31severity\",\"$adpV31vector\",\"$cnaV31score\",\"$cnaV31severity\",\"$cnaV31vector\",\"$cnaV40score\",\"$cnaV40severity\",\"$cnaV40vector\"" >> $outfile
-
